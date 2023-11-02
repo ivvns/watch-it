2f3730cb57f92260efa8559626b2f536afffc3c9
ivvns <ivansomera@hotmail.com>
2023-11-02 13:26:49 -0700

    header styled

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+import '@fortawesome/fontawesome-free/js/all';
 import './style.css'
 
+const search = document.querySelector('.search')
+const submitBtn = document.querySelector('.submit-btn')
+const searchInput = document.querySelector('.search-input')
+
+submitBtn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    searchInput.focus()
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,23 @@ export default {
     "./index.html",
     "./*.{js}",
   ],
+  screen: {
+    sm: '480px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1440px',
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'],
+      },
+      colors: {
+        'bgColor' : '#0D0D0D',
+        'primaryColor' : '#F2F2F2',
+        'secondaryColor' : '#7C27F2',
+      },
+    },
   },
   plugins: [],
 }
